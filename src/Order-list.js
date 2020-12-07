3e692e96baafceb0abb06a3173f2b928c6004a49
brian_wu <brian_wu@symphox.net>
2020-12-07 15:39:16 +0800

    li改成一組，statusType修改結構

--- src/Order-list.js
+++ src/Order-list.js
@@ -1,81 +1,64 @@
 import React, { Component } from "react";
 import productData from './data/product.js';
 
+function OrderItem  (props) {
+  return (<li>
+          <i className="fas fa-angle-right"></i>
+          <div className="img-box">
+            <img src={ props.logo } alt="" />
+          </div>
+          <div className="info-box">
+            <div className="info-title">
+              <span className="order-status">{ props.statusType }</span>
+              {
+                props.status === 1 || props.status === 2 ? 
+                <span className="order-shipment">預計出貨：{ props.date }</span>
+                : null
+              }
+            </div>
+            <div className="info-desc">{ props.name }</div>
+          </div>
+        </li>);
+}
 class OrderList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      album: {},
-    };
+    this.state = {};
     
-  }
-  // componentDidMount = async() => {
-    // const data = await fetch("/json/product.json", {
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Accept: "application/json",
-    //   },
-    // }).then((response) => response.json());
-    // this.setState({
-    //   album: data,
-    // });
-    // console.log(data);
-    // console.log(this.state.album);
-  // };
-  orderList = (status) => {
-    return (status === 'run') ? 
-      productData.productData.map((item) => (
-        (item.status.code === 1 || item.status.code === 2) ?
-          <li>
-            <i className="fas fa-angle-right"></i>
-            <div className="img-box">
-              <img src={ item.logo } alt="" />
-            </div>
-            <div className="info-box">
-              <div className="info-title">
-                <span className="order-status">{ item.status.type }</span>
-                <span className="order-shipment">預計出貨：{ item.date }</span>
-              </div>
-              <div className="info-desc">{ item.name }</div>
-            </div>
-          </li> 
-        : null
-      ))
-      : productData.productData.map((item) => (
-        (item.status.code === 3 || item.status.code === 4) ?
-          <li>
-            <i className="fas fa-angle-right"></i>
-            <div className="img-box">
-              <img src={ item.logo } alt="" />
-            </div>
-            <div className="info-box">
-              <div className="info-title">
-                <span className="order-status">{item.status.type}</span>
-              </div>
-              <div className="info-desc">{ item.name }</div>
-            </div>
-          </li> 
-        : null
-      ))
   }
   render() {
     return (
       <>
         <div className="title">進行中</div>
         <ul className="order-list">
-          { this.orderList('run') }
+          { productData.productData.map((item) => (
+              (item.status.code === 1 || item.status.code === 2) ?
+              < OrderItem 
+                logo={item.logo}
+                statusType={item.status.type}
+                name={item.name}
+                date={item.date}
+                status={item.status.code}
+              /> : null
+            ))
+          }
         </ul>
         <div className="title">已完成</div>
         <ul className="order-list">
-          { this.orderList() }
+          { productData.productData.map((item) => (
+            (item.status.code === 3 || item.status.code === 4) ?
+              < OrderItem 
+                logo={item.logo}
+                statusType={item.status.type}
+                name={item.name}
+                status={item.status.code}
+              /> : null
+            ))
+          }
         </ul>
       </>
     );
   }
 }
-
-// const mapStateToProps = state => ({
-//     name: state.name
-// })
 
 export default OrderList;
