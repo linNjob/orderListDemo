66440ddbea7ed4d65d554b2a15ba42462711ef3a
brian_wu <brian_wu@symphox.net>
2020-12-21 14:58:24 +0800

    改成可控制的內容

--- src/components/containers/orderlist/Order-list.js
+++ src/components/containers/orderlist/Order-list.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+
 import productData from "../../../data/product";
 import OrderItem from "./OrderItem.js";
 import { filterOrdersByStatus } from '../../units/unit';
 
+
 class OrderList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      doing : "1,2",
+      done : "3,4",
+    };
   }
   mapOrderStatus = (items, isDisplayDate) => {
     return items.map((item, index) => {
@@ -14,14 +19,30 @@ class OrderList extends Component {
     })
   }
   render() {
-    const filterOrderIng = filterOrdersByStatus(productData.productData, [1, 2]);
-    const filterOrderDone = filterOrdersByStatus(productData.productData, [3,2,1]);
+    const filterOrderIng = filterOrdersByStatus(productData.productData, this.state.doing);
+    const filterOrderDone = filterOrdersByStatus(productData.productData, this.state.done);
     return (
       <>
-        <div className="title">進行中</div>
+        <div className="title">進行中 
+          <input type="text" 
+                value={this.state.doing} 
+                onChange={(event) => {
+                  this.setState({
+                    doing: event.target.value
+                  })
+                }}/>
+        </div>
         <ul className="order-list">{ this.mapOrderStatus(filterOrderIng, true) }</ul>
         
-        <div className="title">已完成</div>
+        <div className="title">已完成 
+          <input type="text" 
+                value={this.state.done}
+                onChange={(event) => {
+                  this.setState({
+                    done: event.target.value
+                  })
+                }} />
+        </div>
         <ul className="order-list">{ this.mapOrderStatus(filterOrderDone, false) }</ul>
       </>
     );
